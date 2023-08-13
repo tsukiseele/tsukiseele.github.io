220905b2154a24be1b9e39dd930fcccb4d9270b3
tsukiseele <me@hlo.li>
2023-08-13 14:30:19 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,10 @@ export default {
     port: process.env.BASE_PORT || 3000,
   },
    router: {
-    // mode: 'history'
     mode: process.env.MODE && process.env.MODE == 'static' ? 'hash' : 'history'
   },
   ssr: !(process.env.MODE && process.env.MODE == 'static'),
-  // target: process.env.MODE && process.env.MODE.toLocaleUpperCase() == 'static' ? 'static' : 'server',
+  // target: process.env.MODE && process.env.MODE.toLocaleLowerCase() == 'static' ? 'static' : 'server',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: config.title,
