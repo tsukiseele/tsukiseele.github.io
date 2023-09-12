0e5b35f4dd4587cb7328b6317607a3333cad130d
nyarray <i@hlo.li>
2023-09-12 08:59:29 +0800

    update

---
+++ components/SLoading/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.s-loading
+  
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
