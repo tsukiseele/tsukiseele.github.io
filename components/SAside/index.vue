18e1ac98c39bb07157994cd966b7427b6522e606
tsukiseele <me@hlo.li>
2024-03-14 02:24:36 +0800

    update: 更新主页

--- components/SAside/index.vue
+++ components/SAside/index.vue
@@ -1,43 +1,35 @@
 <template lang="pug">
-.s-aside(:class='{ "show-menu": showMenu }')
+.s-aside(:class='{ "show-menu": showMenu, homepage: isHomePage }')
   .menu-btn(@click='showMenu = !showMenu')
     SIcon(name='menu')
   .left
     .left-content
-      .title {{ navigation.title }}
-      //- .title
-        span.letter__wrapper(v-for="char in navigation.title.toLowerCase()" )
-          .letter(:class="char")
-      //-  {{ navigation.title }}
-      .subtitle {{ navigation.subtitle }}
+      .content-main
+        .title {{ navigation.title }}
+        .subtitle {{ navigation.subtitle }}
+        .introduction
+          .blockquote
+            SIcon.quote-left(name='quote')
+            .quote-content(v-text='hitokoto.content', :data-from='hitokoto.from')
+            SIcon.quote-right(name='quote')
+        //- nav.nav
+          ul.nav-blocks
+            li.nav-block(v-for='item in navigation.nav', :key='item.name', :class='{ active: item.to == $route.path }', @click='$router.push(item.to)')
+              SIcon(:name='item.icon')
+              //- .nav-name {{ item.name }}
+              //- .nav-underline
+        ul.nav-links
+          li.nav-link-item(v-for='item in navigation.links', :key='item.name')
+            .item__bg
+            a(:href='item.to', target='_blank')
+              SIcon(:name='item.icon')
+        nav.nav-external(@click='onNavClick')
+          ul.nav-external-links
+            li.nav-link(v-for='item in navigation.nav', :key='item.name', :class='{ active: item.to == $route.path }', @click='$router.push(item.to)')
+              SIcon(:name='item.icon')
+      .content-expand
+        p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-      .introduction
-        .blockquote
-          SIcon.quote-left(name='quote')
-          .quote-content(v-text='hitokoto.content', :data-from='hitokoto.from')
-          SIcon.quote-right(name='quote')
-      //- nav.nav
-        ul.nav-menu
-          li.nav-item(v-for='item in navigation.nav', :key='item.name', :class='{ active: item.to == $route.path }' @click="$router.push(item.to)")
-            SIcon(:name='item.icon')
-            .nav-name {{ item.name }}
-            .nav-underline
-
-      nav.nav-external(@click='onNavClick')
-        ul.nav-external-links
-          li.nav-link(v-for='item in navigation.nav', :key='item.name', :class='{ active: item.to == $route.path }', @click='$router.push(item.to)')
-            SIcon(:name='item.icon')
-      //- nav.nav
-        ul.nav-blocks
-          li.nav-block(v-for='item in navigation.nav', :key='item.name', :class='{ active: item.to == $route.path }', @click='$router.push(item.to)')
-            SIcon(:name='item.icon')
-            //- .nav-name {{ item.name }}
-            //- .nav-underline
-      ul.nav-links
-        li.nav-link-item(v-for='item in navigation.links', :key='item.name')
-          .item__bg
-          a(:href='item.to', target='_blank')
-            SIcon(:name='item.icon')
       .decorate-border
         .decorate-main-dotrect
           .decorate-dotrect
@@ -67,15 +59,15 @@ export default {
       from: '',
     },
     showMenu: false,
+    isHomePage: false,
   }),
   computed: {
     ...mapState(useMainStore, ['navigation', 'isMobile']),
   },
   watch: {
-    $route: {
-      handle(nv, ov) {
-        console.log(nv)
-      },
+    '$route.path'(nv, ov) {
+      this.isHomePage = nv == '/'
+      console.log(this.isHomePage)
     },
   },
   methods: {
@@ -127,8 +119,52 @@ export default {
       top: 0;
       bottom: 0;
       width: 30vw;
+      display: flex;
+      flex-direction: row;
+      height: 100vh;
+      padding: 5rem;
       transition: 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
       z-index: 9;
+      .content-main {
+        align-self: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex: 1;
+      }
+      .content-expand {
+        display: none;
+      }
+    }
+  }
+  &.homepage {
+    .left {
+      .left-content {
+        transform: translateX(0%);
+        width: 100vw;
+        opacity: 1;
+        .content-main {
+          flex: 0 0 calc(30vw - 10rem);
+        }
+        .content-expand {
+          flex: 1;
+          display: flex;
+          position: relative;
+          flex-direction: column;
+          // box-sizing: content-box;
+          &::before {
+            content: '';
+            position: absolute;
+            left: -1rem;
+            top: 10%;
+            bottom: 10%;
+            transform: translateX(-100%);
+
+            border-left: 0.25rem dashed white;
+            // border-right: 0.25rem dashed white;
+          }
+        }
+      }
     }
   }
   .right {
